9300a4e4ce132e5a92b2e74e55dc942422c38ad1
Gideon Baik <gideonbaik@gmail.com>
2018-02-03 19:05:30 -0800

    Change time input to dropdown

--- src/components/ScheduleForm.js
+++ src/components/ScheduleForm.js
@@ -9,7 +9,7 @@ class ScheduleForm extends Component {
     this.state = {
       address: '',
       date: '',
-      time: '',
+      time: '00:00 - 01:00',
       phone: ''
     }
 
@@ -18,7 +18,6 @@ class ScheduleForm extends Component {
   }
 
   handleInputChange(event, form) {
-
     if (form === 'address') {
       this.setState({
         address: event.target.value
@@ -53,8 +52,35 @@ class ScheduleForm extends Component {
     return (
       <div>
         Address: <input type='text' onChange={event => this.handleInputChange(event, 'address')}/>
-        Date: <input type='text' onChange={event => this.handleInputChange(event, 'date')}/>
-        Time: <input type='text' onChange={event => this.handleInputChange(event, 'time')}/>
+        Date: 
+        <input type='text' onChange={event => this.handleInputChange(event, 'date')}/>
+        Time:
+        <select onChange={event => this.handleInputChange(event, 'time')}>
+          <option value="00:00">00:00 - 00:59</option>
+          <option value="01:00">01:00 - 01:59</option>
+          <option value="02:00">02:00 - 02:59</option>
+          <option value="03:00">03:00 - 03:59</option>
+          <option value="04:00">04:00 - 04:59</option>
+          <option value="05:00">05:00 - 05:59</option>
+          <option value="06:00">06:00 - 06:59</option>
+          <option value="07:00">07:00 - 07:59</option>
+          <option value="08:00">08:00 - 08:59</option>
+          <option value="09:00">09:00 - 09:59</option>
+          <option value="10:00">10:00 - 10:59</option>
+          <option value="11:00">11:00 - 11:59</option>
+          <option value="12:00">12:00 - 12:59</option>
+          <option value="13:00">13:00 - 13:59</option>
+          <option value="14:00">14:00 - 14:59</option>
+          <option value="15:00">15:00 - 15:59</option>
+          <option value="16:00">16:00 - 16:59</option>
+          <option value="17:00">17:00 - 17:59</option>
+          <option value="18:00">18:00 - 18:59</option>
+          <option value="19:00">19:00 - 19:59</option>         
+          <option value="20:00">20:00 - 20:59</option>
+          <option value="21:00">21:00 - 21:59</option>
+          <option value="22:00">22:00 - 22:59</option>
+          <option value="23:00">23:00 - 23:59</option>     
+        </select>
         Phone Number: <input type='text' onChange={event => this.handleInputChange(event, 'phone')}/>        
         <button onClick={this.handleSubmitRequest}>Save</button>
       </div>
@@ -64,9 +90,8 @@ class ScheduleForm extends Component {
 
 export default ScheduleForm;
 
-{/* <select name="aircraftSizes"
+{/* <select
 value={this.state.aircraftSize}
 onChange={event => this.handleAircraftSize(event)}>
-  <option value="S">S-Small</option>
-  <option value="L">L-Large</option>
+
 </select> */}
