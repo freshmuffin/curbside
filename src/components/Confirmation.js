606980a75e12576afde1b1047508de69f0e6cf5d
Gideon Baik <gideonbaik@gmail.com>
2018-02-04 07:14:18 -0800

    Add simple confirmation page

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
-const Confirmation = ({}) => (
-  <div>
-    Hello World
+const Confirmation = () => (
+  <div className="confirmation">
+    <h1 className="ui header huge center aligned">Thank you</h1>
+    <h3 className="ui header center aligned">Your garbage will be picked up shortly.</h3>
   </div>
 );
 
